b28341d80f0e595f020e5894a59e5051baa527e0
Sudarshan <godlsudarsan@gmail.com>
2026-03-24 19:23:57 +0530

    Deploy site using docs folder

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: "/Stochastic-Gradient-Descent/",
   plugins: [react()],
   server: {
     port: 3000,
